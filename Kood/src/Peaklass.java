1baf5d0fb4103e263f3dd050e951ebac18de339b
Karlerikkanal <73522629+Karlerikkanal@users.noreply.github.com>
2021-04-03 18:32:37 +0300

    Lisasin uued meetodid

--- Kood/src/Peaklass.java
+++ Kood/src/Peaklass.java
@@ -4,18 +4,18 @@ import java.util.stream.Collectors;
 public class Peaklass {
     public static void main(String[] args) {
         Laud mängija = new Laud("Oliver");
-        pront(mängija.laevadePaigutus());
+        mängija.laevadePaigutus();
         Laud arvuti = new Laud("Arvuti");
+        arvuti.laevadePaigutus();
         System.out.println();
-        pront(arvuti.laevadePaigutus());
         int m2ngijaLaevadeArv = 5;
         int arvutiLaevadeArv = 5;
         //Skeem kuidas mänguväli välja näha võiks
-        /*while(m2ngijaLaevadeArv> 0 || arvutiLaevadeArv >0){
+        /*while(m2ngijaLaevadeArv> 0 || arvutiLaevadeArv >0) {
             mängija.pommita(arvuti.getTühiLaud());
-            pront(arvuti.laevadePaigutus());
+            pront(arvuti.getTühiLaud());
             arvuti.arvuti_pommita(mängija.getTühiLaud());
-            pront(mängija.laevadePaigutus());
+            pront(mängija.getTühiLaud());
         }*/
     }
     static void pront(String[][] m){
