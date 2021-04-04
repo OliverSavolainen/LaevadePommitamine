5241df327fada36a06325536e7bba935fd5a4d6e
OliverSavolainen <oliverssavolainen@gmail.com>
2021-04-04 18:50:47 +0300

    Parandasin veidi vigu, aga neid on kindlasti veel

--- Kood/src/Laud.java
+++ Kood/src/Laud.java
@@ -16,49 +16,49 @@ public class Laud {
     }
 
     public String[][] täidaLaud() {
-        String[][] tühiLaud = new String[10][10];
-        for (int i = 0; i < tühiLaud.length; i++) {
-            for (int j = 0; j < tühiLaud.length; j++) {
-                tühiLaud[i][j] = " ";
+        String[][] mänguLaud = new String[10][10];
+        for (int i = 0; i < mänguLaud.length; i++) {
+            for (int j = 0; j < mänguLaud.length; j++) {
+                mänguLaud[i][j] = " ";
             }
 
         }
-        return tühiLaud;
+        return mänguLaud;
     }
 
-    public String[][] laevadePaigutus(String[][] tühiLaud) {
+    public String[][] laevadePaigutus(String[][] mänguLaud) {
         String[] tähistused = {"5", "4", "3", "2"};
 
         for (int i = 5; i > 1; i--) {
             int vert_koordinaat = (int) Math.round(Math.random() * 9);
             int hori_koordinaat = (int) Math.round(Math.random() * 9);
             int suund = (int) Math.round(Math.random() * 3) + 1;
-            while (!paigutusVõimalik(vert_koordinaat, hori_koordinaat, suund, i, tühiLaud)) {
+            while (!paigutusVõimalik(vert_koordinaat, hori_koordinaat, suund, i, mänguLaud)) {
                 vert_koordinaat = (int) Math.round(Math.random() * 9);
                 hori_koordinaat = (int) Math.round(Math.random() * 9);
                 suund = (int) Math.round(Math.random() * 3) + 1;
             }
             String vastavTähistus = tähistused[tähistused.length + 1 - i];
-            tühiLaud[vert_koordinaat][hori_koordinaat] = vastavTähistus;
+            mänguLaud[vert_koordinaat][hori_koordinaat] = vastavTähistus;
             switch (suund) {
                 case 1 -> {
                     for (int j = 1; j < i; j++) {
-                        tühiLaud[vert_koordinaat + j][hori_koordinaat] = vastavTähistus;
+                        mänguLaud[vert_koordinaat + j][hori_koordinaat] = vastavTähistus;
                     }
                 }
                 case 2 -> {
                     for (int j = 1; j < i; j++) {
-                        tühiLaud[vert_koordinaat - j][hori_koordinaat] = vastavTähistus;
+                        mänguLaud[vert_koordinaat - j][hori_koordinaat] = vastavTähistus;
                     }
                 }
                 case 3 -> {
                     for (int j = 1; j < i; j++) {
-                        tühiLaud[vert_koordinaat][hori_koordinaat + j] = vastavTähistus;
+                        mänguLaud[vert_koordinaat][hori_koordinaat + j] = vastavTähistus;
                     }
                 }
                 case 4 -> {
                     for (int j = 1; j < i; j++) {
-                        tühiLaud[vert_koordinaat][hori_koordinaat - j] = vastavTähistus;
+                        mänguLaud[vert_koordinaat][hori_koordinaat - j] = vastavTähistus;
                     }
                 }
 
@@ -66,17 +66,17 @@ public class Laud {
 
 
         }
-        return tühiLaud;
+        return mänguLaud;
     }
 
-    public boolean paigutusVõimalik(int x, int y, int suund, int pikkus, String[][] tühiLaud) {
+    public boolean paigutusVõimalik(int x, int y, int suund, int pikkus, String[][] mänguLaud) {
         switch (suund) {
             case 1 -> {
                 if (x + pikkus > 9) {
                     return false;
                 }
                 for (int j = 0; j < pikkus; j++) {
-                    if (!tühiLaud[x + j][y].equals(" "))
+                    if (!mänguLaud[x + j][y].equals(" "))
                         return false;
                 }
             }
@@ -85,7 +85,7 @@ public class Laud {
                     return false;
                 }
                 for (int j = 0; j < pikkus; j++) {
-                    if (!tühiLaud[x - j][y].equals(" "))
+                    if (!mänguLaud[x - j][y].equals(" "))
                         return false;
                 }
             }
@@ -94,7 +94,7 @@ public class Laud {
                     return false;
                 }
                 for (int j = 0; j < pikkus; j++) {
-                    if (!tühiLaud[x][y + j].equals(" "))
+                    if (!mänguLaud[x][y + j].equals(" "))
                         return false;
                 }
             }
@@ -103,7 +103,7 @@ public class Laud {
                     return false;
                 }
                 for (int j = 0; j < pikkus; j++) {
-                    if (!tühiLaud[x][y - j].equals(" "))
+                    if (!mänguLaud[x][y - j].equals(" "))
                         return false;
                 }
             }
@@ -111,87 +111,103 @@ public class Laud {
         }
         return true;
     }
-    public String[][] pommita(String[][] tühiLaud){
+    public String[][] pommita(String[][] mänguLaud){
         int x;
         int y;
         do{
             System.out.println("Rida: ");
             x = new Scanner(System.in).nextInt();
-        } while (x < 1 || x > tühiLaud.length);
+        } while (x < 1 || x > mänguLaud.length);
         do{
             System.out.println("Veerg: ");
             y = new Scanner(System.in).nextInt();
-        } while (y < 1 || y > tühiLaud.length);
-        if(!tühiLaud[x][y].equals(" ")){
+        } while (y < 1 || y > mänguLaud.length);
+        if(!mänguLaud[x][y].equals(" ")){
             System.out.println("Said pihta!");
-            if (kasPõhjas(tühiLaud, tühiLaud[x][y])){
+            String tähis = mänguLaud[x][y];
+            mänguLaud[x][y] = "X";
+            if (kasPõhjas(mänguLaud, tähis)){
                 System.out.println("Laev põhjas!");
-                if (kasLäbi(tühiLaud)){
+                if (kasLäbi(mänguLaud)){
                     System.out.println("Mäng läbi. Sina võitsid!");
                     return null;
                 }
             }
-            tühiLaud[x][y] = "X";
-            pommita(tühiLaud);
+
+            pommita(mänguLaud);
         }
-        else if(tühiLaud[x][y].equals("X")||tühiLaud[x][y].equals("O")){
+        else if(mänguLaud[x][y].equals("X")||mänguLaud[x][y].equals("O")){
             System.out.println("Oled seda ruutu juba pommitanud! Vali uuesti.");
-            pommita(tühiLaud);
+            pommita(mänguLaud);
         }
         else {
-            tühiLaud[x][y] = "O";
+            mänguLaud[x][y] = "O";
             System.out.println("Lasid mööda!");
         }
-        return tühiLaud;
+        pront(mänguLaud);
+        return mänguLaud;
     }
-
-    public String[][] arvuti_pommita (String[][] tühiLaud){
+    public String[][] arvutiSaiPihta(String[][] mänguLaud,int x,int y){
+        System.out.println("Arvuti sai su laevale pihta!");
+        String tähis = mänguLaud[x][y];
+        mänguLaud[x][y] = "X";
+        pront(mänguLaud);
+        if (kasPõhjas(mänguLaud, tähis)){
+            System.out.println("Laev põhjas!");
+            if (kasLäbi(mänguLaud)){
+                System.out.println("Mäng läbi. Arvuti võitis");
+                return null;
+            }
+        }
+        mänguLaud[x][y] = "X";
+        return mänguLaud;
+    }
+    public String[][] arvuti_pommita (String[][] mänguLaud){
         int x = (int)(Math.random() * (9 + 1));
         int y  = (int)(Math.random() * (9 + 1));
-        if(!tühiLaud[x][y].equals(" ") && !tühiLaud[x][y].equals("O") && !tühiLaud[x][y].equals("x")){
-            System.out.println("Arvuti sai su laevale pihta!");
-            pront(tühiLaud);
-            if (kasPõhjas(tühiLaud, tühiLaud[x][y])){
-                System.out.println("Laev põhjas!");
-                if (kasLäbi(tühiLaud)){
-                    System.out.println("Mäng läbi. Arvuti võitis");
-                    return null;
-                }
+        if(!mänguLaud[x][y].equals(" ") && !mänguLaud[x][y].equals("O") && !mänguLaud[x][y].equals("X")){
+            arvutiSaiPihta(mänguLaud,x,y);
+            x = (int)(Math.random() * (9 + 1));
+            y  = (int)(Math.random() * (9 + 1));
+            while (!mänguLaud[x][y].equals(" ") && !mänguLaud[x][y].equals("O") && !mänguLaud[x][y].equals("X")){
+                arvutiSaiPihta(mänguLaud,x,y);
+                x = (int)(Math.random() * (9 + 1));
+                y  = (int)(Math.random() * (9 + 1));
             }
-            tühiLaud[x][y] = "X";
-            arvuti_pommita(tühiLaud);
         }
-        else if(tühiLaud[x][y].equals("X")||tühiLaud[x][y].equals("O")){
-            arvuti_pommita(tühiLaud);
+        else if(mänguLaud[x][y].equals("X")||mänguLaud[x][y].equals("O")){
+            arvuti_pommita(mänguLaud);
         }
         else {
-            tühiLaud[x][y] = "O";
+            mänguLaud[x][y] = "O";
             System.out.println("Arvuti lasi mööda!");
+
         }
-        return tühiLaud;
+        pront(mänguLaud);
+        return mänguLaud;
     }
     //Meetod mis leiab kas mingit laeva veel esineb lauas
-    public boolean kasLeidub(String element,String[][] tühiLaud){
+    public boolean kasLeidub(String element,String[][] mänguLaud){
         List<String> nimekiri = new ArrayList<String>();
-        for (String[] rida: tühiLaud) {
+        for (String[] rida: mänguLaud) {
             nimekiri.addAll(Arrays.asList(rida));
         }
         return nimekiri.contains(element);
     }
-    public boolean kasPõhjas(String[][] tühiLaud, String laevaOsa){
-        for (int i = 0; i < tühiLaud.length; i++) {
-            for (int j = 0; j < tühiLaud.length; j++) {
-                if (tühiLaud[i][j].equals(laevaOsa)){
+    public boolean kasPõhjas(String[][] mänguLaud, String laevaOsa){
+        for (int i = 0; i < mänguLaud.length; i++) {
+            for (int j = 0; j < mänguLaud.length; j++) {
+                if (mänguLaud[i][j].equals(laevaOsa)){
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean kasLäbi(String[][] tühiLaud){
-        for (int i = 0; i < tühiLaud.length; i++) {
-            for (int j = 0; j < tühiLaud.length; j++) {
-                if (!tühiLaud[i][j].equals("X") || !tühiLaud[i][j].equals("0") ){
+    public boolean kasLäbi(String[][] mänguLaud){
+        for (int i = 0; i < mänguLaud.length; i++) {
+            for (int j = 0; j < mänguLaud.length; j++) {
+                if (!mänguLaud[i][j].equals("X") || !mänguLaud[i][j].equals("0") || !mänguLaud[i][j].equals(" ") ){
                     return false;
                 }
             }
